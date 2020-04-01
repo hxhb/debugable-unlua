7282f93ec0b3e1d46eddbf10cdf0409a18e4352f
hxhb <hxhb@live.com>
2020-04-01 14:07:36 +0800

    export FPaths

---
+++ Source/UnLua/Private/BaseLib/LuaLib_FPaths.cpp
@@ -0,0 +1,68 @@
+// Tencent is pleased to support the open source community by making UnLua available.
+// 
+// Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the MIT License (the "License"); 
+// you may not use this file except in compliance with the License. You may obtain a copy of the License at
+//
+// http://opensource.org/licenses/MIT
+//
+// Unless required by applicable law or agreed to in writing, 
+// software distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and limitations under the License.
+
+#include "UnLuaEx.h"
+#include "LuaCore.h"
+#include "LuaDynamicBinding.h"
+
+static int32 FPaths_Combine(lua_State *L)
+{
+	int32 NumParams = lua_gettop(L);
+	FString FinalPath;
+	for (int32 index = 1; index <= NumParams; ++index)
+	{
+		FString arg = luaL_tolstring(L, index, nullptr);
+		if (FinalPath.IsEmpty())
+		{
+			if(!arg.IsEmpty())
+				FinalPath = arg;
+
+		}
+		if (!arg.IsEmpty())
+			FinalPath = FPaths::Combine(FinalPath, arg);
+	}
+	lua_pushstring(L, TCHAR_TO_ANSI(*FinalPath));
+	return 1;
+}
+
+static const luaL_Reg FPathsLib[] =
+{
+	{ "Combine", FPaths_Combine },
+	{ nullptr, nullptr }
+};
+
+// Export Actor Component
+BEGIN_EXPORT_CLASS(FPaths)
+	ADD_STATIC_FUNCTION_EX("EngineDir", FString, EngineDir)
+	ADD_STATIC_FUNCTION_EX("EngineUserDir", FString, EngineUserDir)
+	ADD_STATIC_FUNCTION_EX("EngineContentDir", FString, EngineContentDir)
+	ADD_STATIC_FUNCTION_EX("EngineConfigDir", FString, EngineConfigDir)
+	ADD_STATIC_FUNCTION_EX("EngineSavedDir", FString, EngineSavedDir)
+	ADD_STATIC_FUNCTION_EX("EnginePluginsDir", FString, EnginePluginsDir)
+	ADD_STATIC_FUNCTION_EX("PlatformExtensionsDir", FString, PlatformExtensionsDir)
+	ADD_STATIC_FUNCTION_EX("RootDir", FString, RootDir)
+	ADD_STATIC_FUNCTION_EX("ProjectDir", FString, ProjectDir)
+	ADD_STATIC_FUNCTION_EX("ProjectUserDir", FString, ProjectUserDir)
+	ADD_STATIC_FUNCTION_EX("ProjectContentDir", FString, ProjectContentDir)
+	ADD_STATIC_FUNCTION_EX("ProjectConfigDir", FString, ProjectConfigDir)
+	ADD_STATIC_FUNCTION_EX("ProjectSavedDir", FString, ProjectSavedDir)
+	ADD_STATIC_FUNCTION_EX("ProjectIntermediateDir", FString, ProjectIntermediateDir)
+	ADD_STATIC_FUNCTION_EX("ProjectPluginsDir", FString, ProjectPluginsDir)
+	ADD_STATIC_FUNCTION_EX("ProjectLogDir", FString, ProjectLogDir)
+	ADD_STATIC_FUNCTION_EX("GetExtension", FString, GetExtension, const FString&, bool)
+	ADD_STATIC_FUNCTION_EX("Split", void, Split,const FString&, FString&, FString&, FString&)
+	ADD_LIB(FPathsLib)
+END_EXPORT_CLASS()
+IMPLEMENT_EXPORTED_CLASS(FPaths)
+
